78d0cfeee255374d3afe19171e308aa88d4f5bda
Sanjay Krishnan <sanjaykrishn@gmail.com>
2017-07-13 22:58:21 -0700

    Updated with a first example

---
+++ examples/example1.py
@@ -0,0 +1,28 @@
+import environ
+
+data = [{'a': 'New Yorks', 'b': 'NY'}, 
+         {'a': 'New York', 'b': 'NY'}, 
+         {'a': 'San Francisco', 'b': 'SF'},
+         {'a': 'San Francisco', 'b': 'SF'},
+         {'a': 'San Jose', 'b': 'SJ'},
+         {'a': 'New York', 'b': 'NY'},
+         {'a': 'San Francisco', 'b': 'SFO'},
+         {'a': 'Berkeley City', 'b': 'Bk'},
+         {'a': 'San Mateo', 'b': 'SMO'},
+         {'a': 'Albany', 'b': 'AB'},
+         {'a': 'San Mateo', 'b': 'SM'}]
+
+import pandas as pd
+df = pd.DataFrame(data)
+
+
+from alphaclean.constraints import OneToOne
+constraint = OneToOne(["a"], ["b"])
+
+
+from alphaclean.search import solve
+dcprogram = solve(df, dependencies=[constraint], evaluations=3)
+
+print(dcprogram.run(df))
+
+print(dcprogram)
