2892d5ced1dd9fea1e83b72ca5e885d5330311a8
sequoia <sequoia@airbears2-10-142-34-33.airbears2.1918.berkeley.edu>
2017-07-21 16:34:56 -0700

    Superficial edits for readability and adherence to PEP 8 style conventions

--- examples/example3.py
+++ examples/example3.py
@@ -1,31 +1,26 @@
+"""
+Example 3: Election data.
+Run this example with `python examples/example3.py` from the `alphaclean`
+directory.
+"""
+
 import environ
 import pandas as pd
 from alphaclean.search import *
 
 df = pd.read_csv('datasets/elections.txt', quotechar='\"', index_col=False)
 
-
-
 from alphaclean.misc import generateCodebook
-
-codes = generateCodebook(df,'contbr_occupation')
+codes = generateCodebook(df, 'contbr_occupation')
 print(codes)
 
-
-
 config = DEFAULT_SOLVER_CONFIG
-config['dependency']['similarity'] = {'contbr_occupation':'semantic'}
+config['dependency']['similarity'] = {'contbr_occupation': 'semantic'}
 config['dependency']['operations'] = [Swap, Delete]
 
-operation = solve(df, [], dependencies=[DictValue('contbr_occupation', codes)], partitionOn='contbr_nm', config=config)
+operation = solve(df, [], dependencies=[DictValue('contbr_occupation', codes)],
+                  partitionOn='contbr_nm', config=config)
 
 print(operation.run(df))
 
 print(operation)
-
-
-
-
-
-
-
