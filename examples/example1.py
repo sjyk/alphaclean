05d8b1304a58f066daa226f9609b66337225879d
sequoia <sequoia@airbears2-10-142-34-33.airbears2.1918.berkeley.edu>
2017-07-21 16:34:25 -0700

    Superficial edits for readability and adherence to PEP 8 style conventions

--- examples/example1.py
+++ examples/example1.py
@@ -1,29 +1,33 @@
+"""
+Example 1: City names.
+Run this example with `python examples/example1.py` from the `alphaclean`
+directory.
+"""
+
 import environ
 
-data = [{'a': 'New Yorks', 'b': 'NY'}, 
-         {'a': 'New York', 'b': 'NY'}, 
-         {'a': 'San Francisco', 'b': 'SF'},
-         {'a': 'San Francisco', 'b': 'SF'},
-         {'a': 'San Jose', 'b': 'SJ'},
-         {'a': 'New York', 'b': 'NY'},
-         {'a': 'San Francisco', 'b': 'SFO'},
-         {'a': 'Berkeley City', 'b': 'Bk'},
-         {'a': 'San Mateo', 'b': 'SMO'},
-         {'a': 'Albany', 'b': 'AB'},
-         {'a': 'San Mateo', 'b': 'SM'}]
+data = [{'a': 'New Yorks',     'b': 'NY'},
+        {'a': 'New York',      'b': 'NY'},
+        {'a': 'San Francisco', 'b': 'SF'},
+        {'a': 'San Francisco', 'b': 'SF'},
+        {'a': 'San Jose',      'b': 'SJ'},
+        {'a': 'New York',      'b': 'NY'},
+        {'a': 'San Francisco', 'b': 'SFO'},
+        {'a': 'Berkeley City', 'b': 'Bk'},
+        {'a': 'San Mateo',     'b': 'SMO'},
+        {'a': 'Albany',        'b': 'AB'},
+        {'a': 'San Mateo',     'b': 'SM'}]
 
 import pandas as pd
 df = pd.DataFrame(data)
 
-
 from alphaclean.constraints import OneToOne
 constraint = OneToOne(["a"], ["b"])
-
 
 from alphaclean.search import solve, DEFAULT_SOLVER_CONFIG
 config = DEFAULT_SOLVER_CONFIG
 config['dependency']['depth'] = 3
-config['dependency']['similarity'] = {'a':'jaccard'}
+config['dependency']['similarity'] = {'a': 'jaccard'}
 
 dcprogram = solve(df, dependencies=[constraint], config=config)
 
